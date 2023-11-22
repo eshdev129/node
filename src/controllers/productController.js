6ba718c4f1d58e69bb9dcbf5030880ed5a98348d
eshdev129 <148087160+eshdev129@users.noreply.github.com>
2023-11-22 11:09:26 -0300

    Add files via upload

---
+++ src/controllers/productController.js
@@ -0,0 +1,20 @@
+import { Pool } from " ../database/index.js";
+
+class ProductController {
+    async creatProduct({nome, descricao, preco, categoria}){
+        const [result] = await pool.query(`INSERT INTO produto (nome, descricao, preco, categoria)
+        VALUES (?, ?, ?, ?)`, [nome, descricao, preco, categoria])
+        const id = result.insertId;
+        return this.listAllProducts()
+    }
+    
+    
+    async listAllProducts() {
+        const [rows] = await pool.query('SELECT * FROM produto')
+
+        return rows;
+    }
+}
+
+export {ProductController}
+
